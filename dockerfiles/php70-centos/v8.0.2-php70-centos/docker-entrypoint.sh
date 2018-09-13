b006a4eda2e248f3ed27fd759ad5428f24accfb3
Nia Tomonaka <nia1012-tmnk@outlook.jp>
2018-09-14 01:46:50 +0900

    [WIP] Add Dockerfile.

---
+++ dockerfiles/php70-centos/v8.0.2-php70-centos/docker-entrypoint.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo "Starting ..."
+
+# Initialization: Copy Concrete5 source files to www root directory.
+if [ ! -e /var/www/html/concrete ]; then
+    cp -a /usr/src/concrete5/* /var/www/html/
+    echo "Initialized!"
+fi
+
+# If the configuration file is empty, restore the file from the backup in the image.
+if [ -f /etc/httpd/conf.d/concrete5.conf ]; then
+    if [ "$(cat /etc/httpd/conf.d/concrete5.conf)" = "" ]; then
+        cp -f /tmp/conf_backup/concrete5.conf /etc//etc/httpd/conf.d/
+        echo "'/etc/httpd/conf.d/concrete5.conf' is restored."
+    fi
+fi
+
+if [ -f /etc/php.ini ]; then
+    if [ "$(cat /etc/php.ini)" = "" ]; then
+        cp -f /tmp/conf_backup/php.ini /etc/
+        echo "'/etc/php.ini' is restored."
+    fi
+fi
+
+echo "OK!"
+
+# Launch Apache2
+exec /usr/sbin/httpd -D FOREGROUND
